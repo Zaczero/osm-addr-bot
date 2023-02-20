a955334bad188fc136b091eb40a1e48c7884786b
Kamil Monicz <kamil@monicz.pl>
2023-02-20 05:02:35 +0100

    Python 3.11, nix, vscode

--- scripts/docker-update.sh
+++ scripts/docker-update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker run --privileged --rm tonistiigi/binfmt --install all && \
-docker buildx build --platform linux/amd64 -t docker.monicz.pl/osm-addr-bot --no-cache --push .
+docker buildx build --platform linux/amd64 -t docker.monicz.pl/osm-addr-bot --push .
